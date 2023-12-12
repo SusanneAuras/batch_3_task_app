25c1072acafb8d573883738211e31841a56df10c
Susanne Auras <susanne.auras1@gmail.com>
2023-12-12 17:44:47 +0100

    2 Aufgaben optimiert

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
@@ -5,42 +5,22 @@ class S4535 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-    Row(children: [
-    )
-            Container(color: Colors.red,height: 100,width: 100),
-            Container(color: Colors.green,height: 100,width: 100),
-
-
-            Container(color: Colors.blue,height: 100,width: 100),
-            Container(color: Colors.yellow,height: 100,width: 100),
-    
-
-
-
-
-
-    ],)
-    
-
-
-
-/*
-
-
-Container
- 1x (100x100) rot
- 1x (100x100) grün
- 1x (100x100) blau
- 1x (100x100) gelb
-
-
-Column
-
-Row
-
-
-
-
- */
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(color: Colors.red, height: 100, width: 100),
+            Container(color: Colors.green, height: 100, width: 100),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(color: Colors.blue, height: 100, width: 100),
+            Container(color: Colors.yellow, height: 100, width: 100),
+          ],
+        ),
+      ],
+    );
+  }
+}
