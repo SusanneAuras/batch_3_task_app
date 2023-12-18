c05ddb49d826e723175ad363b2b2cfd94ef6f4dc
Susanne Auras <susanne.auras1@gmail.com>
2023-12-18 09:36:19 +0100

    Lösung4536

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -7,3 +7,56 @@ class S4536 extends StatelessWidget {
     throw UnimplementedError();
   }
 }
+
+class Product {
+  String name;
+  double price;
+
+  Product({required this.name, required this.price});
+}
+
+class MyApp extends StatelessWidget {
+  final List<Product> products = [
+    Product(name: "Produkt 1", price: 10.99),
+    Product(name: "Produkt 2", price: 20.49),
+    Product(name: "Produkt 3", price: 15.75),
+    Product(name: "Produkt 4", price: 8.99),
+    Product(name: "Produkt 5", price: 25.99),
+  ];
+
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Product List"),
+        ),
+        body: ProductListView(products: products),
+      ),
+    );
+  }
+}
+
+class ProductListView extends StatelessWidget {
+  final List<Product> products;
+
+  const ProductListView({super.key, required this.products});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: const Icon(Icons.shopping_cart),
+          title: Text(products[index].name),
+/*           subtitle: Text('\$${products[index].price.toStringAsFixed(2)}'), */
+          onTap: () {},
+        );
+      },
+    );
+  }
+}
